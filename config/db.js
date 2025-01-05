4a609532e73b05536bb829be0ce2bed1ecbbaa07
ajucp <ajmalcp4@gmail.com>
2025-01-06 04:08:46 +0530

    add mongodb newly

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoClient=mongodb.MongoClient;
 let _db;
 
 const mongoConnect=async(callback)=>mongoClient.connect(
-    'mongodb+srv://ajucp:J6GQDsaCXT2uo2Ga@cluster0.98nsj.mongodb.net/url-shorten-app?retryWrites=true&w=majority'
+    'mongodb+srv://ajmalcp:AuJT5T4gmA4kLiNT@cluster0.98nsj.mongodb.net/node-shop?retryWrites=true&w=majority'
      )
     .then(client=>{
         console.log("DATA_BASE IS CONNECTED");
