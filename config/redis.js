19bd7be3cce728465e731d41b117bfffc16247b1
ajucp <ajmalcp4@gmail.com>
2025-01-06 03:43:49 +0530

    add redis

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,7 @@
 const redis=require('redis');
 
 const redisClient=redis.createClient({
+    url: process.env.REDIS_URL,
     socket: {
         host: 'redis', // or '127.0.0.1'
         port: 6379,
